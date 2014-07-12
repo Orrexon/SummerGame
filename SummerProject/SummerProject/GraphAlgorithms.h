c9025d3cb0bba7fe5c020c945698a9fb3c32880c
Orrexon <orrexon@gmail.com>
2014-07-12 19:25:00 +0200

    forgot to save

--- SummerProject/SummerProject/GraphAlgorithms.h
+++ SummerProject/SummerProject/GraphAlgorithms.h
@@ -6,3 +6,255 @@
 #include <stack>
 
 #include "SparseGraph.h"
+#include "PriorityQueue.h"
+
+//Class to implement a deapth first search, DFS
+template<class graph_type>
+class Graph_SearchDFS
+{
+private:
+	enum {visited, unvisited, no_parent_assigned};
+
+	//typedefs for the edge and node types used by the graph
+	typedef typename graph_type::EdgeType Edge;
+	typedef typename graph_type::NodeType Node;
+
+public:
+	Graph_SearchDFS(const graph_type& graph,
+		int source,
+		int target = -1)
+		:m_Graph(graph),
+		m_iSource(source),
+		m_iTarget(target),
+		m_bFound(false),
+		m_Visited(m_graph.numNodes(), unvisited),
+		m_Route(m_Graph.numNodes(), no_parent_assigned)
+	{
+		m_bFound = Search();
+	}
+	//return the edge vector
+	std::vector<const Edge*> getSearchTree()const { return m_SpanningTree; }
+
+	bool Found()const { return m_bFound; }
+
+	//returns a vector of node indexes that comprise the shortest path
+	//from source to target
+	std::list<int> getPathToTarget()const;
+private:
+	//a ref to the graph to be searched
+	const graph_type& m_Graph;
+
+	//this records the indexes of all the nodes that are visited
+	//as the search progresses
+	std::vector<int> m_Visited;
+
+	//this holds the route taken to the target
+	std::vector<int> m_Route;
+
+	//as the this holds all the edges examined.
+	//THIS IS ONLY USEFUL IF YOU WANT TO DRAW THE EXAMINED EDGES
+	std::vector<const Edge*> m_SpanningTree;
+
+	int m_iSource, m_iTarget;
+
+	bool m_bFound;
+
+	//This performs the DFS search
+	bool Search();
+};
+
+template<class graph_type>
+class Graph_SearchBFS
+{
+private:
+
+	enum { visited, unvisited, no_parent_assigned };
+
+	//create a typedef for the edge type used by the graph
+	typedef typename graph_type::EdgeType Edge;
+
+public:
+	Graph_SearchBFS(const graph_type& graph,
+		int source,
+		int target = -1)
+		:m_Graph(graph),
+		m_iSource(source),
+		m_iTarget(target),
+		m_bFound(false),
+		m_Visited(m_graph.numNodes(), unvisited),
+		m_Route(m_Graph.numNodes(), no_parent_assigned)
+	{
+		m_bFound = Search();
+	}
+	//return the edge vector
+	std::vector<const Edge*> getSearchTree()const { return m_SpanningTree; }
+
+	bool Found()const { return m_bFound; }
+
+	//returns a vector of node indexes that comprise the shortest path
+	//from source to target
+	std::list<int> getPathToTarget()const;
+private:
+	//a ref to the graph to be searched
+	const graph_type& m_Graph;
+
+	//this records the indexes of all the nodes that are visited
+	//as the search progresses
+	std::vector<int> m_Visited;
+
+	//this holds the route taken to the target
+	std::vector<int> m_Route;
+
+	//as the this holds all the edges examined.
+	//THIS IS ONLY USEFUL IF YOU WANT TO DRAW THE EXAMINED EDGES
+	std::vector<const Edge*> m_SpanningTree;
+
+	int m_iSource, m_iTarget;
+
+	bool m_bFound;
+
+	//This performs the BFS search is similar to the DFS except It uses
+	//a first in first out queue instead of a stack
+	bool Search();
+};
+
+//Graph Search Dijkstra
+//Shortest path from source to target
+
+template<class graph_type>
+class Graph_SearchDijkstra
+{
+private:
+
+	typedef typename graph_type::EdgeType Edge;
+
+public:
+	Graph_SearchDijkstra(const graph_type& graph,
+		int source,
+		int target = -1)
+		:m_Graph(graph),
+		m_ShortestPathTree(graph.numNodes()),
+		m_SearchFrontier(graph.numNodes()),
+		m_CostToThisNode(graph.numNodes()),
+		m_iSource(source),
+		m_iTarget(target)
+	{
+		Search();
+	}
+
+	//returns the vector of edges that defines the SPT. If a target was given
+	//in the constructor then this will be an SPT comprising of all the nodes
+	//examined before the target was found, else it will contain all the nodes
+	//in the graph.
+	std::vector<const Edge*> GetSPT()const{ return m_ShortestPathTree; }
+
+	//returns a vector of node indexes that comprise the shortest path
+	//from the source to the target. It calculates the path by working
+	//backwards through the SPT from the target node.
+	std::list<int> getPathToTarget()const;
+
+	//returns the total cost to the target
+	double getCostToTarget()const{ return m_CostToThisNode[m_iTarget]; }
+
+	//returns the total cost to the given node
+	double getCostToNode(unsigned int nd)const{ return m_CostToThisNode[nd]; }
+
+private:
+	const graph_type& m_Graph;
+
+	//this vector contains the edges that comprise the shortest path tree -
+	//a directed subtree of the graph that encapsulates the best paths from 
+	//every node on the SPT to the source node.
+	std::vector<const Edge*>      m_ShortestPathTree;
+
+	//this is indexed into by node index and holds the total cost of the best
+	//path found so far to the given node. For example, m_CostToThisNode[5]
+	//will hold the total cost of all the edges that comprise the best path
+	//to node 5, found so far in the search (if node 5 is present and has 
+	//been visited)
+	std::vector<double>            m_CostToThisNode;
+
+	//this is an indexed (by node) vector of 'parent' edges leading to nodes 
+	//connected to the SPT but that have not been added to the SPT yet. This is
+	//a little like the stack or queue used in BST and DST searches.
+	std::vector<const Edge*>     m_SearchFrontier;
+
+	int                           m_iSource;
+	int                           m_iTarget;
+
+	void Search();
+};
+
+//Graph A-Star
+template<class graph_type, class heuristic>
+class Graph_SearchAStar
+{
+private:
+
+	//create a typedef for the edge type used by the graph
+	typedef typename graph_type::EdgeType Edge;
+
+public:
+	Graph_SearchAStar(graph_type& graph,
+		int source,
+		int target = -1)
+		:m_Graph(graph),
+		m_ShortestPathTree(graph.numNodes()),
+		m_SearchFrontier(graph.numNodes()),
+		m_GCosts(graph.numNodes(), 0.0),
+		m_FCosts(graph.numNodes(), 0.0),
+		m_iSource(source),
+		m_iTarget(target)
+	{
+		Search();
+	}
+
+	//returns the vector of edges that the algorithm has examined
+	std::vector<const Edge*> GetSPT()const{ return m_ShortestPathTree; }
+
+	//returns a vector of node indexes that comprise the shortest path
+	//from the source to the target
+	std::list<int> getPathToTarget()const;
+
+	//returns the total cost to the target
+	double getCostToTarget()const{ return m_GCosts[m_iTarget]; }
+
+private:
+
+	const graph_type&              m_Graph;
+
+	//indexed into my node. Contains the 'real' accumulative cost to that node
+	std::vector<double>             m_GCosts;
+
+	//indexed into by node. Contains the cost from adding m_GCosts[n] to
+	//the heuristic cost from n to the target node. This is the vector the
+	//iPQ indexes into.
+	std::vector<double>             m_FCosts;
+
+	std::vector<const Edge*>       m_ShortestPathTree;
+	std::vector<const Edge*>       m_SearchFrontier;
+
+	int                            m_iSource;
+	int                            m_iTarget;
+
+	//the A* search algorithm
+	void Search();
+
+};
+
+class Heuristic_Euclid
+{
+public:
+
+	Heuristic_Euclid(){}
+
+	//calculate the straight line distance from node nd1 to node nd2
+	template <class graph_type>
+	static double Calculate(const graph_type& G, int nd1, int nd2)
+	{
+		sf::Vector2f vFrom = G.GetNode(nd1).getPosition(););
+		sf::Vector2f vTo = G.GetNode(nd2).getPosition());
+		sf::Vector2f vDist = vTo - vFrom;
+		return sqrt(vDist.x*vDist.x + vDist.y*vDist.y);
+	}
+};
---
+++ SummerProject/SummerProject/GraphFunctions.h
@@ -0,0 +1,299 @@
+//GraphFunctions.h
+
+#pragma once
+#include <iostream>
+#include <SFML\Graphics.hpp>
+#include "GraphAlgorithms.h"
+#include "MathNerd.h"
+
+//  returns true if x,y is a valid position in the map
+bool ValidNeighbour(int x, int y, int NumCellsX, int NumCellsY)
+{
+	return !((x < 0) || (x >= NumCellsX) || (y < 0) || (y >= NumCellsY));
+}
+
+//  use to add he eight neighboring edges of a graph node that 
+//  is positioned in a grid layout
+template <class graph_type>
+void GraphHelper_AddAllNeighboursToGridNode(graph_type& graph,
+	int row,
+	int col,
+	int NumCellsX,
+	int NumCellsY)
+{
+	for (int i = -1; i<2; ++i)
+	{
+		for (int j = -1; j<2; ++j)
+		{
+			int nodeX = col + j;
+			int nodeY = row + i;
+
+			//skip if equal to this node
+			if ((i == 0) && (j == 0)) continue;
+
+			//check to see if this is a valid neighbour
+			if (ValidNeighbour(nodeX, nodeY, NumCellsX, NumCellsY))
+			{
+				//calculate the distance to this node
+				sf::Vector2i PosNode = graph.GetNode(row*NumCellsX + col).getPosition();
+				sf::Vector2i PosNeighbour = graph.GetNode(nodeY*NumCellsX + nodeX).getPosition();
+
+				sf::Vector2i vDist = PosNeighbour - PosNode;
+				double dist = sqrtf(vDist.x * vDist.x - vDist.y * vDist.y);
+
+				//this neighbour is okay so it can be added
+				graph_type::EdgeType NewEdge(row*NumCellsX + col,
+					nodeY*NumCellsX + nodeX,
+					dist);
+				graph.AddEdge(NewEdge);
+
+				//if graph is not a diagraph then an edge needs to be added going
+				//in the other direction
+				if (!graph.isDigraph())
+				{
+					graph_type::EdgeType NewEdge(nodeY*NumCellsX + nodeX,
+						row*NumCellsX + col,
+						dist);
+					graph.AddEdge(NewEdge);
+				}
+			}
+		}
+	}
+}
+
+//  creates a graph based on a grid layout. This function requires the 
+//  dimensions of the environment and the number of cells required horizontally
+//  and vertically 
+
+template <class graph_type>
+void GraphHelper_CreateGrid(graph_type& graph,
+	int cySize,
+	int cxSize,
+	int NumCellsY,
+	int NumCellsX)
+{
+	//need some temporaries to help calculate each node center
+	double CellWidth = (double)cySize / (double)NumCellsX;
+	double CellHeight = (double)cxSize / (double)NumCellsY;
+
+	double midX = CellWidth / 2;
+	double midY = CellHeight / 2;
+
+
+	//first create all the nodes
+	for (int row = 0; row<NumCellsY; ++row)
+	{
+		for (int col = 0; col<NumCellsX; ++col)
+		{
+			graph.addNode(NavGraphNode<>(graph.getNextFreeNodeIndex(),
+				sf::Vector2f(midX + (col*CellWidth),
+				midY + (row*CellHeight))));
+
+		}
+	}
+	//now to calculate the edges. (A position in a 2d array [x][y] is the
+	//same as [y*NumCellsX + x] in a 1d array). Each cell has up to eight
+	//neighbours.
+	for (int row = 0; row<NumCellsY; ++row)
+	{
+		for (int col = 0; col<NumCellsX; ++col)
+		{
+			GraphHelper_AddAllNeighboursToGridNode(graph, row, col, NumCellsX, NumCellsY);
+		}
+	}
+}
+
+//  draws a graph using the GDI
+//TO DO: Fix an sfml draw function instead of GDI
+template <class graph_type>
+void GraphHelper_DrawUsingGDI(const graph_type& graph, int color, bool DrawNodeIDs = false)
+{
+
+	//just return if the graph has no nodes
+	if (graph.NumNodes() == 0) return;
+
+	gdi->SetPenColor(color);
+
+	//draw the nodes 
+	graph_type::ConstNodeIterator NodeItr(graph);
+	for (const graph_type::NodeType* pN = NodeItr.begin();
+		!NodeItr.end();
+		pN = NodeItr.next())
+	{
+		gdi->Circle(pN->Pos(), 2);
+
+		if (DrawNodeIDs)
+		{
+			gdi->TextColor(200, 200, 200);
+			gdi->TextAtPos((int)pN->Pos().x + 5, (int)pN->Pos().y - 5, ttos(pN->Index()));
+		}
+
+		graph_type::ConstEdgeIterator EdgeItr(graph, pN->Index());
+		for (const graph_type::EdgeType* pE = EdgeItr.begin();
+			!EdgeItr.end();
+			pE = EdgeItr.next())
+		{
+			gdi->Line(pN->Pos(), graph.GetNode(pE->To()).Pos());
+		}
+	}
+}
+
+//  Given a cost value and an index to a valid node this function examines 
+//  all a node's edges, calculates their length, and multiplies
+//  the value with the weight. Useful for setting terrain costs.
+
+template <class graph_type>
+void WeightNavGraphNodeEdges(graph_type& graph, int node, double weight)
+{
+	//make sure the node is present
+	assert(node < graph.numNodes());
+
+	//set the cost for each edge
+	graph_type::ConstEdgeIterator ConstEdgeItr(graph, node);
+	for (const graph_type::EdgeType* pE = ConstEdgeItr.begin();
+		!ConstEdgeItr.end();
+		pE = ConstEdgeItr.next())
+	{
+		//calculate the distance between nodes
+		sf::Vector2f vFrom = graph.getNode(pE->From().getPosition());
+		sf::Vector2f vTo = graph.getNode(pE->To().getPosition());
+		sf::Vector2f vDist = vTo - vFrom;
+
+		double dist = sqrtf(vDist.x*vDist.x + vDist.y*vDist.y);
+
+		//set the cost of this edge
+		graph.setEdgeCost(pE->From(), pE->To(), dist * weight);
+
+		//if not a digraph, set the cost of the parallel edge to be the same
+		if (!graph.isDigraph())
+		{
+			graph.setEdgeCost(pE->To(), pE->From(), dist * weight);
+		}
+	}
+}
+
+// creates a lookup table encoding the shortest path info between each node
+// in a graph to every other
+template <class graph_type>
+std::vector<std::vector<int> > CreateAllPairsTable(const graph_type& G)
+{
+	enum { no_path = -1 };
+
+	std::vector<int> row(G.numNodes(), no_path);
+
+	std::vector<std::vector<int> > ShortestPaths(G.numNodes(), row);
+
+	for (int source = 0; source<G.numNodes(); ++source)
+	{
+		//calculate the SPT for this node
+		Graph_SearchDijkstra<graph_type> search(G, source);
+
+		std::vector<const graph_type::EdgeType*> spt = search.GetSPT();
+
+		//now we have the SPT it's easy to work backwards through it to find
+		//the shortest paths from each node to this source node
+		for (int target = 0; target<G.NumNodes(); ++target)
+		{
+			//if the source node is the same as the target just set to target
+			if (source == target)
+			{
+				ShortestPaths[source][target] = target;
+			}
+
+			else
+			{
+				int nd = target;
+
+				while ((nd != source) && (spt[nd] != 0))
+				{
+					ShortestPaths[spt[nd]->From][target] = nd;
+
+					nd = spt[nd]->From;
+				}
+			}
+		}//next target node
+	}//next source node
+
+	return ShortestPaths;
+}
+
+//  creates a lookup table of the cost associated from traveling from one
+//  node to every other
+
+template <class graph_type>
+std::vector<std::vector<double> > CreateAllPairsCostsTable(const graph_type& G)
+{
+	//create a two dimensional vector
+	std::vector<double> row(G.numNodes(), 0.0);
+	std::vector<std::vector<double> > PathCosts(G.numNodes(), row);
+
+	for (int source = 0; source<G.numNodes(); ++source)
+	{
+		//do the search
+		Graph_SearchDijkstra<graph_type> search(G, source);
+
+		//iterate through every node in the graph and grab the cost to travel to
+		//that node
+		for (int target = 0; target<G.numNodes(); ++target)
+		{
+			if (source != target)
+			{
+				PathCosts[source][target] = search.GetCostToNode(target);
+			}
+		}//next target node
+
+	}//next source node
+
+	return PathCosts;
+}
+
+//  determines the average length of the edges in a navgraph (using the 
+//  distance between the source & target node positions (not the cost of the 
+//  edge as represented in the graph, which may account for all sorts of 
+//  other factors such as terrain type, gradients etc)
+template <class graph_type>
+double CalculateAverageGraphEdgeLength(const graph_type& G)
+{
+	double TotalLength = 0;
+	int NumEdgesCounted = 0;
+
+	graph_type::ConstNodeIterator NodeItr(G);
+	const graph_type::NodeType* pN;
+	for (pN = NodeItr.begin(); !NodeItr.end(); pN = NodeItr.next())
+	{
+		graph_type::ConstEdgeIterator EdgeItr(G, pN->Index());
+		for (const graph_type::EdgeType* pE = EdgeItr.begin(); !EdgeItr.end(); pE = EdgeItr.next())
+		{
+			//increment edge counter
+			++NumEdgesCounted;
+
+			//add length of edge to total length
+			sf::Vector2f vFrom = G.getNode(pE->From().getPosition(););
+			sf::Vector2f vTo = G.getNode(pE->To().getPosition());
+			sf::Vector2f vDist = vTo - vFrom;
+			double length = sqrtf(vDist.x*vDist.x + vDist.y*vDist.y);
+			TotalLength += length;
+		}
+	}
+
+	return TotalLength / (double)NumEdgesCounted;
+}
+//  returns the cost of the costliest edge in the graph
+template <class graph_type>
+double GetCostliestGraphEdge(const graph_type& G)
+{
+	double greatest = MinDouble;
+
+	graph_type::ConstNodeIterator NodeItr(G);
+	const graph_type::NodeType* pN;
+	for (pN = NodeItr.begin(); !NodeItr.end(); pN = NodeItr.next())
+	{
+		graph_type::ConstEdgeIterator EdgeItr(G, pN->Index());
+		for (const graph_type::EdgeType* pE = EdgeItr.begin(); !EdgeItr.end(); pE = EdgeItr.next())
+		{
+			if (pE->Cost() > greatest)greatest = pE->Cost();
+		}
+	}
+
+	return greatest;
+}
